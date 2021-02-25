24f742293a5a142f95efc83a7b7a567dae831063
KvGM <Resis235@gmail.com>
2021-02-25 21:21:06 +0000

    Formularios y arreglo en el flujo

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const url = './mocks/products.json';
 
 const starterZone = document.getElementsByClassName('starter')[0];
 const consumedProducts = document.getElementById('consumedProducts');
+const nav = document.getElementsByTagName('nav')[0];
 
 window.addEventListener('load', () => {
 
@@ -20,7 +21,7 @@ document.getElementById("toogleForm").addEventListener('click', e => {
 
 document.getElementById("startBtn").addEventListener('click', async (e) => {
     await new Promise(r => {
-        setTimeout(r, 1000);
+        setTimeout(r, 3000);
         starterZone.children[2].style.display = "none";
         loader(starterZone, true);
         starterZone.children[0].disabled = true;
@@ -36,7 +37,8 @@ document.getElementById("startBtn").addEventListener('click', async (e) => {
             starterZone.parentNode.appendChild((showProduct(product)));
         });
         consumedProducts.classList.add('droppable');
-        //mostrar Formulario
+        nav.style.display = "block";
+        loadEvents();
         loadJquery();
     }
 })
@@ -51,6 +53,7 @@ function showProduct(product) {
 }
 
 function sumProperties() {
+    console.clear();
     let fats = 0;
     let hydrates = 0;
     let proteins = 0;
@@ -88,5 +91,137 @@ function loadJquery() {
             $(ui.draggable[0]).draggable("option", "revert", false);
             $(ui.draggable[0]).draggable("disable");
         }
+    })
+}
+
+function validateCode(target) {
+    let errorZone = target.nextElementSibling;
+    target.classList.remove('success-field');
+    target.classList.remove('error-field');
+    errorZone.textContent = '';
+    let existProduct = productList.find(product => target.value == product.id);
+    if (target.validity.valueMissing) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'Campo obligatorio.';
+    } else if (target.validity.patternMismatch) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'El código ha de ser un número de tres cifras que empiece por 1.';
+    } else if (typeof existProduct != 'undefined') {
+        target.classList.add('error-field');
+        errorZone.textContent = 'Este código ya existe, pruebe con otro.';
+    } else {
+        target.classList.add('success-field');
+    }
+}
+
+function validateName(target) {
+    let errorZone = target.nextElementSibling;
+    target.classList.remove('success-field');
+    target.classList.remove('error-field');
+    errorZone.textContent = '';
+    if (target.validity.valueMissing) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'Campo obligatorio.';
+    } else if (target.validity.patternMismatch) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'El nombre ha de tener más de 3 letras.';
+    } else {
+        target.classList.add('success-field');
+    }
+}
+
+function validateProperties(target) {
+    let errorZone = target.nextElementSibling;
+    target.classList.remove('success-field');
+    target.classList.remove('error-field');
+    errorZone.textContent = '';
+    if (target.validity.valueMissing) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'Campo obligatorio.';
+    } else if (target.value < 1) {
+        target.classList.add('error-field');
+        errorZone.textContent = 'El valor mínimo es 1.';
+    } else {
+        target.classList.add('success-field');
+    }
+}
+
+async function validateForm() {
+    let fails = 0;
+    let code = document.getElementById('code');
+    let name = document.getElementById('name');
+    let fat = document.getElementById('fat');
+    let hydrates = document.getElementById('hydrates');
+    let protein = document.getElementById('protein');
+    if (!code.validity.valid) {
+        validateCode(code);
+        fails++;
+    }
+    if (!name.validity.valid) {
+        validateName(name);
+        fails++;
+    }
+    if (!fat.validity.valid) {
+        validateProperties(fat);
+        fails++;
+    }
+    if (!hydrates.validity.valid) {
+        validateProperties(hydrates);
+        fails++;
+    }
+    if (!protein.validity.valid) {
+        validateProperties(protein);
+        fails++;
+    }
+    if (fails == 0) {
+        let product = {
+            id: code.value,
+            name: name.value,
+            grasas: fat.value,
+            hidratos: hydrates.value,
+            proteinas: protein.value
+        }
+        await new Promise(r => {
+            setTimeout(r, 3000);
+            productList.push(product);
+            code.value = '';
+            name.value = '';
+            fat.value = '';
+            hydrates.value = '';
+            protein.value = '';
+            code.classList.remove('success-field');
+            name.classList.remove('success-field');
+            fat.classList.remove('success-field');
+            hydrates.classList.remove('success-field');
+            protein.classList.remove('success-field');
+            document.getElementsByTagName('form')[0].children[2].firstElementChild.disabled = true;
+            document.getElementsByTagName('form')[0].children[2].firstElementChild.textContent = 'Cargando...';
+        });
+        starterZone.parentNode.appendChild((showProduct(product)));
+        document.getElementsByTagName('form')[0].children[2].firstElementChild.disabled = false;
+        document.getElementsByTagName('form')[0].children[2].firstElementChild.textContent = 'Añadir';
+        loadJquery();
+    }
+}
+
+function loadEvents() {
+    document.getElementById('code').addEventListener('input', (e) => {
+        validateCode(e.target);
+    });
+    document.getElementById('name').addEventListener('input', (e) => {
+        validateName(e.target);
+    });
+    document.getElementById('fat').addEventListener('input', (e) => {
+        validateProperties(e.target);
+    });
+    document.getElementById('hydrates').addEventListener('input', (e) => {
+        validateProperties(e.target);
+    });
+    document.getElementById('protein').addEventListener('input', (e) => {
+        validateProperties(e.target);
+    });
+    document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
+        validateForm();
+        e.preventDefault();
     })
 }
