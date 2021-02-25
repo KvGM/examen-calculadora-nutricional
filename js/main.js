f4d79b71178eac9dbd7f21e892d333f088c5954d
KvGM <Resis235@gmail.com>
2021-02-25 20:02:55 +0000

    Jquery y calculo de propiedades

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import {
 const url = './mocks/products.json';
 
 const starterZone = document.getElementsByClassName('starter')[0];
+const consumedProducts = document.getElementById('consumedProducts');
 
 window.addEventListener('load', () => {
 
@@ -18,13 +19,15 @@ document.getElementById("toogleForm").addEventListener('click', e => {
 })
 
 document.getElementById("startBtn").addEventListener('click', async (e) => {
-    /* Usar esta sentencia para ralentizar la petición ajax*/
     await new Promise(r => {
         setTimeout(r, 1000);
+        starterZone.children[2].style.display = "none";
         loader(starterZone, true);
+        starterZone.children[0].disabled = true;
     });
     productList = await getProducts(url);
     if (productList == null) {
+        starterZone.children[0].disabled = false;
         starterZone.children[1].style.display = "none";
         starterZone.children[2].style.display = "block";
     } else {
@@ -32,16 +35,34 @@ document.getElementById("startBtn").addEventListener('click', async (e) => {
         productList.forEach(product => {
             starterZone.parentNode.appendChild((showProduct(product)));
         });
+        consumedProducts.classList.add('droppable');
+        //mostrar Formulario
+        loadJquery();
     }
 })
 
 function showProduct(product) {
     let template = document.getElementsByTagName('template')[0].content.cloneNode(true);
-    let agrupador = template.firstElementChild;
-    agrupador.textContent = product.name;
-    agrupador.dataset.code = product.id;
-    agrupador.classList.add('draggable');
-    return agrupador;
+    let div = template.firstElementChild;
+    div.textContent = product.name;
+    div.dataset.code = product.id;
+    div.classList.add('draggable');
+    return div;
+}
+
+function sumProperties() {
+    let fats = 0;
+    let hydrates = 0;
+    let proteins = 0;
+    selectedProducts.forEach(product => {
+        fats += parseInt(product.grasas);
+        hydrates += parseInt(product.hidratos);
+        proteins += parseInt(product.proteinas);
+    })
+    console.log('Información nutricional:');
+    console.log(`Grasas: ${fats}`);
+    console.log(`Hidratos: ${hydrates}`);
+    console.log(`Proteínas: ${proteins}`);
 }
 
 function loader(zone, option) {
@@ -51,5 +72,21 @@ function loader(zone, option) {
         zone.children[1].style.display = "none";
         zone.style.display = "none";
     }
+}
 
+function loadJquery() {
+    $(".draggable").draggable({
+        containment: "#interactive",
+        revert: true
+    });
+    $(".droppable").droppable({
+        drop: function (event, ui) {
+            let draggableCode = ui.draggable[0].dataset.code;
+            let product = productList.find(prodct => draggableCode == prodct.id);
+            selectedProducts.push(product);
+            sumProperties();
+            $(ui.draggable[0]).draggable("option", "revert", false);
+            $(ui.draggable[0]).draggable("disable");
+        }
+    })
 }
